61d9375ee79625eabda9dd94f86da4c71924c211
guillaume.collombet@gmail.com <guillaume.collombet@gmail.com@d4d2d3c1-6ae0-3e48-a65f-f74f1b6baf28>
2012-10-29 15:19:47 +0000

    Ajout de l'include vers la classe automate

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -1,8 +1,11 @@
-#include<iostream>
+#include <iostream>
+#include "automate.h"
+
 using namespace std;
 
 int main(int argc, char** argv)
 {
+    Automate* automate = new Automate();
     cout << "Hello world!";
     return 0;
 }
