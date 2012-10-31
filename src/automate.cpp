a433a34919c16df280878f6070f36856cfe0ca05
guillaume.collombet <guillaume.collombet@gmail.com>
2012-10-31 17:15:10 +0000

    Codind of delta method and in/out stream operator of Automat class Commenting of Automate class

--- src/automate.cpp
+++ src/automate.cpp
@@ -2,47 +2,132 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
-bool Automate::readfile(const string filename)
+Automate::Automate()
 {
-    //TODO: Add a comment
-    //TODO: Complete the reading method
-    ifstream file(filename.c_str(), ios::in);
-    if(file)
-    {
-        file >> nb_etats >> nb_symboles >> nb_etats_finaux;
-        file >> etat_initial;
-        for (int i = 0; i< nb_etats_finaux; i++)
-        {
-            int var;
-            file >> var;
-            etats_finaux.insert(etats_finaux.end(), var);
-        }
-        //initialisation de la matrice
-        matrice_transition.resize(nb_etats);
-        for(int i = 0; i<nb_etats; i++ )
-            matrice_transition[i].resize(nb_symboles);
-        int etat,etat_arrive;
-        char symbole;
-        for(int i = 0; i<nb_etats*nb_symboles; i++)
-        {
-              file >> etat >> symbole >> etat_arrive;
-         matrice_transition[etat][symbole - 97] = etat_arrive;
-        }
-         file.close();
-        return true;
-    }
-    else  // sinon
-                    cerr << "Impossible d'ouvrir le fichier !" << endl;
-
-    return false;
+    this->etats_finaux = set<int>();
+    this->etat_initial = 0;
+    this->nb_etats = 0;
+    this->nb_etats_finaux = 0;
+    this->nb_symboles = 0;
+    this->matrice_transition = vector<vector<int>>();
 }
 
-pair<int, bool> Automate::delta(const string str)
+Automate::~Automate()
 {
+    delete etats_finaux;
+    delete matrice_transition;
+}
 
-    //TODO: Add a comment
+Automate* Automate::readfile(const char* filename)
+{
+    ifstream file (filename);
+    if (file.is_open())
+    {
+      string line;
+      string data;
+      while ( file.good() )
+      {
+        getline (file,line);
+        data += line + "\n";
+      }
+      file.close();
+      Automate* value = new Automate();
+      *value << data.c_str();
+      return value;
+    }
+    else
+        return (Automate*)0;
+}
+
+ostream& operator<<(ostream& stream, const Automate& value)
+{
+    stream << value.nb_etats << " " << value.nb_symboles << " " << value.etats_finaux.size() << endl;
+    stream << value.etat_initial << endl;
+    for(set<int>::iterator i = value.etats_finaux.begin(); i != value.etats_finaux.end(); i++)
+        stream << *i << endl;
+    for(int i = 0; i < value.nb_etats; i++)
+    {
+        for(int j = 0; j < value.nb_symboles; j++)
+        {
+            stream << i << " " << j << " " << value.matrice_transition[i][j] << endl;
+        }
+    }
+    return stream;
+}
+
+istream& operator>> (istream& stream, Automate& value)
+{
+    string blank;
+    stream >> value.nb_etats >> blank >> value.nb_symboles >> blank >> value.nb_etats_finaux >> blank;
+    stream >> value.etat_initial >> blank;
+    int state, symbole, rstate;
+    for(int i = 0; i < value.nb_etats_finaux; i++)
+    {
+        stream >> state >> blank;
+        value.etats_finaux.insert(state);
+    }
+    vector<int> line = vector<int>();
+    for(int i = 0; i < value.nb_etats; i++)
+    {
+        line = vector<int>();
+        line.assign(value.nb_symboles,0);
+    }
+    for(int i = 0; i < value.nb_etats * value.nb_symboles; i++)
+    {
+        stream >> state >> blank >> symbole >> blank >> rstate >> blank;
+        value.matrice_transition[state][symbole] = rstate;
+    }
+    return stream;
+}
+
+Automate& Automate::operator <<(const char* data)
+{
+    char* reader = (char*)data;
+    // lecture du nombre d'états, de symboles et d'états finaux
+    reader += sscanf_s(reader,"%d %d %d\n", &this->nb_etats, &this->nb_symboles, &this->nb_etats_finaux);
+
+    for(int i = 0; i < this->nb_etats; i++)
+    {
+        vector<int> line = vector<int>();
+        line.assign(this->nb_symboles,0);
+        this->matrice_transition.push_back(line);
+    }
+
+    // lecture de l'état initial
+    reader += sscanf_s(reader, "%d\n", &this->etat_initial);
+
+    // lecture des états finaux
+    for (int i=0; i<this->nb_etats_finaux; i++) {
+      int e;
+      reader += sscanf_s(reader, "%d\n", &e);
+      // e : état final courant
+      this->etats_finaux.insert(e);
+    }
+
+    // lecture de la matrice delta
+    int e, ne; char c; // (e,c,ne) : transition courante
+    reader += sscanf_s(reader, "%d %c %d\n", &e, &c,sizeof(c), &ne);
+    while (reader != nullptr) {
+      reader += sscanf_s(reader, "%d %c %d\n", &e, &c,sizeof(c), &ne);
+      this->matrice_transition[e][(int)c] = ne;
+    }
+
+    return *this;
+}
+
+pair<int, bool> Automate::delta(const int state, const char symbole) const
+{
     //TODO: Test
+    int rstate = this->matrice_transition[state][symbole];
+    set<int>::iterator i;
+    for(i = etats_finaux.begin();i != etats_finaux.end();i++)
+    {
+        if(*i == rstate)
+            return pair<int, bool>(rstate,true);
+    }
+    return pair<int, bool>(rstate,false);
 }
--- src/automate.h
+++ src/automate.h
@@ -2,33 +2,54 @@
 #define AUTOMATE_H
 
 #include <vector>
-#include <string>
+#include <set>
+#include <iostream>
 
 using namespace std;
 
-//TODO: Add a comment
-class Automate
-{
-  // caractristiques
-private :
+/*!
+ * \brief Contient les attribut et méthode permetant la définition et l'execution d'un automate finit deterministe
+ */
+class Automate{
+  // caractéristiques
   int nb_etats;
   int nb_symboles;
   int nb_etats_finaux;
 
-  // tats initial
+  // états initial
   int etat_initial;
 
-  // tats finaux
-  vector<int> etats_finaux;
+  // états finaux
+  set<int> etats_finaux;
 
   // matrice de transition
-  vector<vector<int> > matrice_transition;
+  vector<vector<int>> matrice_transition;
 
 public:
-  //TODO: Add a comment
-  bool readfile(const string filename);
-  //TODO: Add a comment
-  pair<int,bool> delta(const string str);
+  static Automate *readfile(const char* filename);
+
+  explicit Automate();
+  ~Automate();
+
+  /*!
+   * \brief operator << operateur de flux de sortie permetant la sérialition de l'automate passé en argument dans le flux à gauche de l'opérande
+   */
+  friend ostream& operator<< (ostream& stream, const Automate& value);
+  /*!
+   * \brief operator >> opérateur de flux d'entrée permetant la désérialisation dans l'automate passé en argument à partir du flux à gauche de l'opérande
+   */
+  friend istream& operator>> (istream& stream, Automate& value);
+
+  Automate& operator<< (const char* data);
+
+  /*!
+   * \brief delta fonction de transition de l'automate qui retourne un etat de sortie en fonction d'un etat et d'un symbole donné en entrée.
+   * \param state etat courant de l'automate
+   * \param symbole symoble à lire par l'automate
+   * \return pair<int,bool> representant le numéro de l'état de sortie et un booléen indiquant si celui ci est final ou non
+   */
+  pair<int,bool> delta(const int state, const char symbole) const;
+
 
 };
 
