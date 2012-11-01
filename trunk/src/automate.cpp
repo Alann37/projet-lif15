1963c69366df8b549768b1d8c51728450cd43ade
guillaume.collombet@gmail.com <guillaume.collombet@gmail.com@d4d2d3c1-6ae0-3e48-a65f-f74f1b6baf28>
2012-11-01 22:59:16 +0000

    Mise en place de la lecture de chaine dans le main Ajout de trace du déroulement de la lecture dans l'operateur << de lecture de chaine dans l'automate Test et identification de deux bug

--- trunk/src/automate.cpp
+++ trunk/src/automate.cpp
@@ -10,10 +10,43 @@ Automate::Automate()
 {
     _finalStates = set<int>();
     _initialState = 0;
-    _statesCount = 0;
-    _finalStatesCount = 0;
-    _symbolesCount = 0;
-    matrice_transition = vector<vector<int>>();
+    _statesCount = 1;
+    _finalStatesCount = 1;
+    _symbolesCount = 1;
+    _transitionMatrix = vector<vector<int>>();
+}
+
+Automate::Automate(int myInitialState,const set<int>& myFinalStates,const vector<vector<int>>& myTransitionMaxtrix)
+{
+    vector<int> z;
+    if(myTransitionMaxtrix.empty())
+        throw underflow_error(string("L'automate doit contenir au moins un état."));
+
+    vector<int> state = *myTransitionMaxtrix.begin();
+    for(vector<vector<int>>::const_iterator i = (myTransitionMaxtrix.begin() + 1); i != myTransitionMaxtrix.end(); i++)
+    {
+        if(state.size() != (*i).size())
+            throw invalid_argument("L'automate n'est pas deterministe. La matrice de transition n'est pas un tableau à deux dimension, des lignes de taille différentes ont été trouvées.");
+    }
+
+    if(myTransitionMaxtrix[0].empty())
+        throw underflow_error(string("L'automate doit contenir au moins un symbole par état."));
+
+    if((myInitialState < 0) || (myInitialState >= (int)myTransitionMaxtrix.size()))
+        throw overflow_error(string("L'état initial ne fait pas partit de l'automate."));
+
+    for(set<int>::iterator i = myFinalStates.begin(); i != myFinalStates.end(); i++)
+    {
+        if((*i < 0) || (*i >= (int)myTransitionMaxtrix.size()))
+            throw overflow_error(string("Un ou plusieurs états finaux ne font pas partit de l'automate."));
+    }
+
+    _finalStates = myFinalStates;
+    _initialState = myInitialState;
+    _transitionMatrix = myTransitionMaxtrix;
+    _symbolesCount = myTransitionMaxtrix[0].size();
+    _statesCount = myTransitionMaxtrix.size();
+    _finalStatesCount = myFinalStates.size();
 }
 
 Automate::~Automate()
@@ -59,16 +92,18 @@ bool Automate::isFinal() const
 
 int Automate::delta(const int state, const char symbole) const
 {
-    return this->matrice_transition[state][(int)symbole - 97];
+    return this->_transitionMatrix[state][(int)symbole - 97];
 }
 
 Automate& Automate::operator<< (const string& value)
 {
+    cout << "L'automate est dans l'état " << _currentState << ", il va lire la chaine de caractère : '" << value << "'" << endl;
     for( string::const_iterator i = value.begin(); i != value.end(); i++)
     {
         if((((int)*i - 97) < 0) || (((int)*i - 97) > _symbolesCount))
             throw overflow_error(string("Le symbole ") + *i + string(" n'est pas accepté par l'automate"));
         _currentState = delta(currentState(),*i);
+        cout << "L'automate lit le caractère " << *i << " et passe dans l'état " << _currentState << endl;
     }
     return *this;
 }
@@ -83,7 +118,7 @@ ostream& operator<<(ostream& stream, const Automate& value)
     {
         for(int j = 0; j < value._symbolesCount; j++)
         {
-            stream << i << " " << j << " " << value.matrice_transition[i][j] << endl;
+            stream << i << " " << j << " " << value._transitionMatrix[i][j] << endl;
         }
     }
     return stream;
@@ -103,13 +138,14 @@ istream& operator>> (istream& stream, Automate& value)
     vector<int> line = vector<int>();
     for(int i = 0; i < value._statesCount; i++)
     {
-        line = vector<int>();
+        line.clear();
         line.assign(value._symbolesCount,0);
+        value._transitionMatrix.push_back(line);
     }
     for(int i = 0; i < value._statesCount * value._symbolesCount; i++)
     {
         stream >> state >> blank >> symbole >> blank >> rstate >> blank;
-        value.matrice_transition[state][symbole] = rstate;
+        value._transitionMatrix[state][symbole] = rstate;
     }
     return stream;
 }
